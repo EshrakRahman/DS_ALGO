da858a5629f204f6131aa86b8fc3c76f4edc3823
Eshrak Rahman <eshrakurrahman@gmail.com>
2020-12-03 19:31:57 +0600

    Initial commit

---
+++ src/main/java/com/dsalgo/eshrak/GameEntry.java
@@ -0,0 +1,23 @@
+package com.dsalgo.eshrak;
+
+public class GameEntry {
+    private String name;
+    private int score;
+
+    public GameEntry (String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String toString() {
+        return "(" + name + ", " + score + ")";
+    }
+}
